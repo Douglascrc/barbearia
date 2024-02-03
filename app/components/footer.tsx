53d8c3fc22112d37743f9c9ed06f2034fe50b28b
Douglascrc <douglasbuscam@gmail.com>
2024-02-03 19:54:05 -0300

    feat: add footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="w-full py-6 px-5 bg-secondary ">
+            <h1 className="text-gray-400 opacity-75 text-xs font-bold">© 2023 Copywrite Barber_DG</h1>
+        </div>
+     );
+}
+ 
+export default Footer;
