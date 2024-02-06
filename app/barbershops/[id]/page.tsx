37c3dc7ef3ae89fd44f0061a5dad1b43c15ee208
Douglascrc <douglasbuscam@gmail.com>
2024-02-06 20:18:12 -0300

    feat: images of service in component "service-item"

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/app/lib/prisma";
 import BarbershopInfo from "./components/barbershop-info";
+import ServiceItem from "./components/service-item";
 
 interface BarbershopDetailsPageProps {
     params: {
@@ -24,7 +25,14 @@ const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
         return null
     } 
 
-    return <BarbershopInfo barbershop={barbershop}/>
+    return (
+        <div className="">
+             <BarbershopInfo barbershop={barbershop}/>
+            {barbershop.services.map((service) => (
+                <ServiceItem key={service.id} service={service}/>
+            ))}
+        </div>
+    )
 }
  
 export default BarbershopDetailsPage;
