c378f04a49f0828c1ca273df76d20d402cc6e1cd
Douglascrc <douglasbuscam@gmail.com>
2024-02-10 18:42:10 -0300

    Style: add margin top

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-        <div className="w-full py-6 px-5 bg-secondary ">
+        <div className="w-full py-6 px-5 mt-4 bg-secondary ">
             <h1 className="text-gray-400 opacity-75 text-xs font-bold">© 2023 Copywrite Barber_DG</h1>
         </div>
      );
