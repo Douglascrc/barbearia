cb637fd8cd8f0c2e2df5e020789b209eca3c715a
Douglascrc <douglasbuscam@gmail.com>
2024-03-14 22:43:12 -0300

    feat: add booking page

--- app/components/booking-item.tsx
+++ app/components/booking-item.tsx
@@ -1,29 +1,41 @@
-import { format } from "date-fns";
+import { format, isFuture, isPast } from "date-fns";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
 import { Card, CardContent } from "./ui/card";
 import { ptBR } from "date-fns/locale";
+import { Prisma } from "@prisma/client";
 
-const BookingItem = () => {
+interface BarbershopItemProps {
+    booking: Prisma.BookingGetPayload<{
+        include: {
+            service: true,
+            barbershop: true,
+        }
+    }>
+}
+
+const BookingItem = ({booking}: BarbershopItemProps) => {
+    const isBookingConfirmed = isFuture(booking.date)
+
     return ( 
         <Card>
             <CardContent className="flex py-0 px-0">
                 <div className="flex flex-col gap-2 py-5 flex-[3] pl-5">
-                    <Badge className="text-white bg-[#221C3D] text-primary hover:bg-[#221C3D] w-fit"> Confirmado </Badge>
+                    <Badge variant={isBookingConfirmed ?"secondary": "default"} className="w-fit"> {isBookingConfirmed ? "Confirmado" : "Finalizado" } </Badge>
                     <h2> Corte de Cabelo </h2>
 
                     <div className="flex items-center gap-2">
                         <Avatar className="h-9 w-9">
-                            <AvatarImage src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png"/>
+                            <AvatarImage src={booking.barbershop.imageUrl}/>
                             <AvatarFallback>A</AvatarFallback>
                         </Avatar>
 
-                        <h2 className="text-sm">Vintage Barber</h2>
+                        <h2 className="text-sm">{booking.barbershop.name}</h2>
                     </div>
                 </div>
 
                 <div className="flex flex-col justify-center px-3 items-center border-l ">
-                    <p className="text-sm"> {format(new Date(), "MMMM", {
+                    <p className="text-sm capitalize"> {format(new Date(), "MMMM", {
             locale:ptBR,
             })} </p>
                     <p className="text-xl">{format(new Date(), "dd", {
